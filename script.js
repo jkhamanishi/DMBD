7ebffe900254b95af87bc3a79f0425e53c434ca4
jkhamanishi <jkhamanishi@gmail.com>
2023-09-30 21:11:05 -0700

    Added hover details

--- script.js
+++ script.js
@@ -26,18 +26,44 @@ function csvToObject(csv, delimiter=',') {
     });
 };
 
-const SIZE = 72;
+// Box pixel size in portrait orientation
+const BOX_WIDTH = 72;
+const BOX_HEIGHT = BOX_WIDTH*(4/3);
 
 function createBox(box) {
+    box.details = "Box #"+box.box_number+": "+box.name+" ("+box.year+")";
     const wall = document.getElementById("wall");
     const boxContainer = createAndAppendElement(wall, "div", "box-container");
     boxContainer.id = "box-" + box.box_number;
-    boxContainer.style.width = (box.wall_orientation == "portrait" ? SIZE : SIZE*(4/3)) + "px";
-    const gridWidth = box.wall_orientation == "portrait" ? 6 : 8;
-    boxContainer.style.gridColumn = box.column + " / span " + gridWidth;
-    boxContainer.style.gridRow = - box.row;  // row 1 is at the bottom
-    const boxImage = createAndAppendElement(boxContainer, "img", "box-image");
+    const boxDetailsContainer = createAndAppendElement(boxContainer, "div", "box-details-container bg-primary");
+    const boxImage = createAndAppendElement(boxDetailsContainer, "img", "box-image");
     boxImage.src = "../data/images/box_previews/box (" + box.box_number + ").jpg";
+    if (box.wall_orientation == "portrait") {
+        boxContainer.style.width = BOX_WIDTH + "px";
+        boxContainer.style.height = BOX_HEIGHT + "px";
+        boxContainer.style.gridColumn = box.column + " / span 6";
+        boxImage.width = BOX_WIDTH;
+        boxImage.height = BOX_HEIGHT;
+    } else if (box.wall_orientation == "landscape") {
+        boxContainer.style.width = BOX_HEIGHT + "px";
+        boxContainer.style.height = BOX_WIDTH + "px";
+        boxContainer.style.gridColumn = box.column + " / span 8";
+        boxImage.width = BOX_HEIGHT;
+        boxImage.height = BOX_WIDTH;
+    } else {
+        throw new Error("Box " + box.box_number + " has an wall invalid orientation: " + box.wall_orientation);
+    }
+    boxContainer.style.gridRow = - box.row;  // row 1 is at the bottom
+
+    const boxDetails = document.createElement("p");
+    boxDetails.className = "box-details text-light mb-0";
+    boxDetails.innerText = box.details;
+    $(boxContainer).hover(function() {
+        boxDetailsContainer.appendChild(boxDetails);
+    }, function() {
+        boxDetailsContainer.removeChild(boxDetails);
+    });
+
 }
 
 function createAndAppendElement(parent, tagName, classes) {
